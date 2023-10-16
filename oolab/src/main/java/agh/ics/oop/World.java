20b7dc785045257bc80f34d7667925dd7c9b8bb1
mbartnicki80 <mbartnicki@student.agh.edu.pl>
2023-10-16 23:05:09 +0200

    Fixed run method

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -3,18 +3,36 @@ import agh.ics.oop.model.MoveDirection;
 
 public class World {
     static void start() {
-        System.out.println("System wystartował");
+        System.out.println("System wystartowal");
     }
     static void stop() {
-        System.out.println("System zakończył działanie");
+        System.out.println("System zakonczyl dzialanie");
     }
+
+    /*static void run(String[] args) {
+        System.out.println("Zwierzak idzie do przodu");
+        for (int i=0; i<args.length; i++) {
+            System.out.print(args[i]);
+            if (i != args.length-1)
+                System.out.print(", ");
+        }
+        System.out.print("\n");
+        for (int i=0; i<args.length; i++) {
+            switch (args[i]) {
+                case "f" -> System.out.println("Zwierzak idzie do przodu");
+                case "b"  -> System.out.println("Zwierzak idzie do tylu");
+                case "r"  -> System.out.println("Zwierzak skreca w prawo");
+                case "l"  -> System.out.println("Zwierzak skreca w lewo");
+            }
+        }
+    }*/
     static void run(MoveDirection[] directions) {
         for (int i=0; i<directions.length; i++) {
             switch (directions[i]) {
                 case FORWARD -> System.out.println("Zwierzak idzie do przodu");
-                case BACKWARD  -> System.out.println("Zwierzak idzie do tyłu");
-                case RIGHT  -> System.out.println("Zwierzak skręca w prawo");
-                case LEFT  -> System.out.println("Zwierzak skręca w lewo");
+                case BACKWARD  -> System.out.println("Zwierzak idzie do tylu");
+                case RIGHT  -> System.out.println("Zwierzak skreca w prawo");
+                case LEFT  -> System.out.println("Zwierzak skreca w lewo");
             }
         }
     }
